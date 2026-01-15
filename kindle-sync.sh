5def6e98c7ad464ea8d476ab5fd22524ad7f0034
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2026-01-15 23:21:38 +0100

    Update kindle-sync.sh to change local sync directory from ~/kindle-sync/ to ~/synkindle/

--- kindle-sync.sh
+++ kindle-sync.sh
@@ -11,7 +11,7 @@
 
 export PATH="/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
-/usr/local/bin/rclone bisync kindle:/mnt/us/documents/books/ ~/kindle-sync/ \
+/usr/local/bin/rclone bisync kindle:/mnt/us/documents/books/ ~/synkindle/ \
     -v \
     --compare size,modtime \
     --conflict-resolve newer
